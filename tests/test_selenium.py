2934764c7712939b9585bd3a9a7fab0eaf4baff6
Dennis Buathong <dennis.buathong@hotmail.com>
2025-01-19 15:17:11 +0100

    Playwright Testing

--- tests/test_selenium.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-import pytest
-
-def test_selenium():
-    driver = webdriver.Chrome
-    driver.get("http://10.0.24.60:30000/camunda/app/welcome/default/#!/login")
---
+++ tests/tests/Anmeldeformular.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Anmeldeformular', async ({ page }) => {
+  await page.goto('http://10.0.24.77:30000/camunda/app/welcome/default/#!/login');
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Username').press('Tab');
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.locator('.tasklist-app > .app-label-wrapper').click();
+  await page.getByRole('link', { name: ' Start process' }).click();
+  await page.getByRole('link', { name: 'Garantiefallprozess' }).click();
+  await page.reload();
+  await page.getByRole('button', { name: 'Start' }).click();
+  await page.getByRole('heading', { name: 'All Tasks' }).click();
+  await page.locator('li').filter({ hasText: 'Gerätedaten erfassen Garantiefallprozess Created a few seconds ago' }).getByRole('link').click();
+  await page.getByRole('button', { name: 'Claim' }).click();
+  await page.getByLabel('DeviceModel Test').click();
+  await page.getByLabel('DeviceModel Test').fill('Test1');
+  await page.getByRole('button', { name: 'Complete' }).click();
+});
