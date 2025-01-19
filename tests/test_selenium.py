a06313c20ee73cfcebacf00ff4d4ec7ecee71d5c
Dennis Buathong <dennis.buathong@hotmail.com>
2025-01-19 14:42:49 +0100

    Test Playwright container

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 import pytest
 
 def test_selenium():
-    driver = webdriver.Edge
-    driver.get("https:")
+    driver = webdriver.Chrome
+    driver.get("http://10.0.24.60:30000/camunda/app/welcome/default/#!/login")
